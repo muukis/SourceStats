e5304cee5b1881d9441930266244afc920885358
Mikko Andersson <stranger@pilssi.net>
2017-12-16 17:37:39 +0200

    Log4Net

--- SourceStatsServices/SourceStatsServices/SourceStatsServices/Controllers/SourceStatsController.cs
+++ SourceStatsServices/SourceStatsServices/SourceStatsServices/Controllers/SourceStatsController.cs
@@ -1,18 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using log4net;
 using Microsoft.AspNetCore.Mvc;
 using SourceStatsServices.Models;
+using System;
+using System.Net;
 
 namespace SourceStatsServices.Controllers
 {
     [Route("api/[controller]")]
     public class SourceStatsController : Controller
     {
+        private static readonly ILog _logger;
+
+        static SourceStatsController()
+        {
+            _logger = LogManager.GetLogger(typeof(SourceStatsController));
+        }
+
         [HttpPost]
         public void Post([FromBody]SaveCommand saveCommand)
         {
+            _logger.Info($"Save command received: {saveCommand}");
+
+            try
+            {
+                // Todo: Validate server identity token
+                // Todo: Save stats using entity framework
+            }
+            catch (Exception e)
+            {
+                _logger.Fatal("Save command failed.", e);
+                throw new WebException("Save command failed.");
+            }
         }
     }
 }
---
+++ SourceStatsServices/SourceStatsServices/SourceStatsServices/Support/Logger.cs
@@ -0,0 +1,27 @@
+﻿using log4net;
+using log4net.Config;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace SourceStatsServices.Support
+{
+    public static class Logger
+    {
+        static Logger()
+        {
+            var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
+            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
+        }
+
+        public static ILog GetLogger<T>(this T obj) where T : class
+        {
+            return LogManager.GetLogger(typeof(T));
+        }
+
+        public static ILog GetLogger(Type type)
+        {
+            return LogManager.GetLogger(type);
+        }
+    }
+}
