84a9d3ad471651c5e2e36eac772ab3ba41495287
Mikko Andersson <stranger@pilssi.net>
2017-12-16 16:56:45 +0200

    Comments for command base class

--- SourceStatsServices/SourceStatsServices/SourceStatsServices/Models/CommandBase.cs
+++ SourceStatsServices/SourceStatsServices/SourceStatsServices/Models/CommandBase.cs
@@ -5,8 +5,14 @@ using System.Threading.Tasks;
 
 namespace SourceStatsServices.Models
 {
+    /// <summary>
+    /// Command base for all different command types
+    /// </summary>
     public abstract class CommandBase
     {
+        /// <summary>
+        /// Gets or sets server identity token (must match the available tokens to save)
+        /// </summary>
         public string ServerIdentityToken { get; set; }
     }
 }
